c6ecee33d6d78de475284fb432926e271fabbd3d
Willian Souza <wrdigital@hotmail.com>
2022-08-25 00:26:05 -0300

    add swagger to paginate request

--- src/infra/database/typeorm/requests/paginate.request.ts
+++ src/infra/database/typeorm/requests/paginate.request.ts
@@ -1,25 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class PaginateRequest {
+  @ApiProperty({
+    required: false,
+  })
   @IsOptional()
   @IsString()
   search: string;
 
+  @ApiProperty({
+    required: false,
+  })
   @IsOptional()
   @IsString()
   type: string;
 
+  @ApiProperty({
+    required: false,
+    default: 1,
+  })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
   page: number;
 
+  @ApiProperty({
+    required: false,
+    default: '-id',
+  })
   @IsOptional()
   @IsString()
   sort: string;
 
+  @ApiProperty({
+    required: false,
+    default: 15,
+  })
   @IsOptional()
   @Type(() => Number)
   @Min(5)
