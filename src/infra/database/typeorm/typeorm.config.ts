cde34f4366c3be2faa1faf8f55df9a99bae27479
Willian Souza <wrsystem@gmail.com>
2022-08-27 03:37:13 -0300

    feat: add database in memory config

--- src/infra/database/typeorm/typeorm.config.ts
+++ src/infra/database/typeorm/typeorm.config.ts
@@ -6,13 +6,24 @@ import {
 
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   useFactory: (configService: ConfigService) =>
-    ({
-      type: configService.get<string>('CONNECTION_TYPE'),
-      url: configService.get<string>('CONNECTION_STRING'),
-      entities: [`${__dirname}/../../../**/*.entity{.js,.ts}`],
-      synchronize: false,
-      logging:
-        configService.get<string>('NODE_ENV') !== 'production' ? true : false,
-    } as TypeOrmModuleOptions),
+    configService.get<string>('NODE_ENV') !== 'test'
+      ? ({
+          type: configService.get<string>('CONNECTION_TYPE'),
+          url: configService.get<string>('CONNECTION_STRING'),
+          entities: [`${__dirname}/../../../**/*.entity{.js,.ts}`],
+          synchronize: false,
+          logging:
+            configService.get<string>('NODE_ENV') !== 'production'
+              ? true
+              : false,
+        } as TypeOrmModuleOptions)
+      : ({
+          type: 'better-sqlite3',
+          database: ':memory:',
+          entities: [`${__dirname}/../../../**/*.entity{.js,.ts}`],
+          synchronize: true,
+          logging: false,
+          dropSchema: true,
+        } as TypeOrmModuleOptions),
   inject: [ConfigService],
 };
